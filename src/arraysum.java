ea67081e378175b8460d6a3743696c2dce55a33d
janhavi mane <manejanhavi198@gmail.com>
2025-09-18 19:12:39 +0530

    "Initial commit"

--- src/arraysum.java
+++ src/arraysum.java
@@ -1,5 +1,5 @@
 import java.util.Scanner;
-public class array1{
+public class arraysum{
     public static void main(String[]args) {
         Scanner sc = new Scanner(System.in);
         System.out.println("enter size of array:");
